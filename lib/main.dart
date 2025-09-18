53c9c0540136067b1c09e6de0c828a0e770996c1
Alex-Yu21 <yualex2112@gmail.com>
2025-09-18 15:38:02 +0200

    feat(app): wire GoRouter via ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,35 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flashcards_v2/app/navigation/router.dart';
 import 'package:flashcards_v2/app/theme/theme_controller.dart';
+import 'package:flashcards_v2/features/auth/domain/entities/session.dart';
+import 'package:flashcards_v2/features/auth/presentation/providers/auth_providers.dart';
+import 'package:flashcards_v2/firebase_options.dart';
 import 'package:flutter/material.dart';
-
-import 'firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final theme = ThemeController();
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const App());
+
+  runApp(const ProviderScope(child: AppRoot()));
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class AppRoot extends ConsumerWidget {
+  const AppRoot({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final session = ref.watch(authControllerProvider);
+
+    Session getCurrentSession() => ref.read(authControllerProvider);
+
+    final router = createRouter(
+      initial: session,
+      currentSession: getCurrentSession,
+    );
+
     return ValueListenableBuilder<CustomTheme?>(
       valueListenable: theme,
       builder: (_, __, ___) {
@@ -27,22 +39,6 @@ class App extends StatelessWidget {
           darkTheme: theme.darkTheme,
           themeMode: theme.themeMode,
           routerConfig: router,
-          // home:
-          // StreamBuilder(
-          //   stream: FirebaseAuth.instance.authStateChanges(),
-          //   builder: (ctx, snapshot) {
-          //     if (snapshot.connectionState == ConnectionState.waiting) {
-          //       return const LoadView();
-          //     }
-          //     if (snapshot.hasData) {
-          //       return const HomeView();
-          //     } else {
-          //       return
-          // const AuthView(),
-          // ;
-          //     }
-          //   },
-          // ),
         );
       },
     );
