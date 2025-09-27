0b63b866da0df552772cc46ae06734b403ece905
Alex-Yu21 <yualex2112@gmail.com>
2025-09-27 14:26:35 +0200

    chore; learning view template

--- lib/features/learning/presentation/views/learning_view.dart
+++ lib/features/learning/presentation/views/learning_view.dart
@@ -8,24 +8,71 @@ class LearningView extends StatefulWidget {
 }
 
 class _LearningViewState extends State<LearningView> {
+  void onKnow() {}
+  void onDontKnow() {}
+  void onReveal() {}
+
   @override
   Widget build(BuildContext context) {
+    const iconSize = 50.00;
     return Scaffold(
-      body: Column(
-        children: [
-          // CardSwiper(
-          //   cardsCount: 10,
-          //   cardBuilder:
-          //       (
-          //         context,
-          //         index,
-          //         horizontalOffsetPercentage,
-          //         verticalOffsetPercentage,
-          //       ) => Card(),
-          // ),
-        ],
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // TODO прогресс бар
+            Center(child: SizedBox(width: 350, height: 100, child: Card())),
+            SizedBox(height: 100),
+            // TODO заменить плейсхолдер на настоящие карточки
+            Center(child: SizedBox(width: 350, height: 200, child: Card())),
+            SizedBox(height: 100),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(14),
+                  ),
+                  onPressed: onKnow,
+                  child: const Icon(Icons.done, size: iconSize),
+                ),
+                SizedBox(width: 100),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(14),
+                  ),
+                  onPressed: onDontKnow,
+                  child: const Icon(Icons.close, size: iconSize),
+                ),
+              ],
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(12),
+              ),
+              onPressed: () {},
+              child: const Icon(Icons.remove_red_eye, size: iconSize),
+            ),
+
+            // CardSwiper(
+            //   cardsCount: 10,
+            //   cardBuilder:
+            //       (
+            //         context,
+            //         index,
+            //         horizontalOffsetPercentage,
+            //         verticalOffsetPercentage,
+            //       ) => Card(),
+            // ),
+          ],
+        ),
       ),
     );
   }
 }
- // TODO" спрятать навбар 
