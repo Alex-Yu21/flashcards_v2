27c0ebff52c0f379883413a2507521d028919ec8
Alex-Yu21 <yualex2112@gmail.com>
2025-10-02 12:42:49 +0200

    refactor(Learning): split FlashcardEntity into LanguageCardEntity and CodeCardEntity

--- lib/features/learning/domain/entities/flashcarde.dart
+++ lib/features/learning/domain/entities/flashcarde.dart
@@ -1,23 +1,54 @@
 import 'package:flashcards_v2/features/learning/domain/entities/card_category.dart';
 
-class FlashcardEntity {
+abstract class FlashcardEntity {
   FlashcardEntity({
     required this.id,
     required this.title,
-    required this.translation,
-    this.transcription,
-    this.audioPath,
-    this.example,
-    this.description,
     this.category = CardCategory.newWords,
   });
 
   final String id;
   final String title;
+  CardCategory category;
+}
+
+final class LanguageCardEntity extends FlashcardEntity {
+  LanguageCardEntity({
+    required super.id,
+    required super.title,
+    super.category = CardCategory.newWords,
+
+    required this.translation,
+    this.transcription,
+    this.audioPath,
+    this.example,
+    this.description,
+  });
+
   final String translation;
   final String? transcription;
   final String? audioPath;
   final String? description;
   final String? example;
-  CardCategory category;
 }
+
+final class CodeCardEntity extends FlashcardEntity {
+  CodeCardEntity({
+    required super.id,
+    required super.title,
+    super.category = CardCategory.newWords,
+
+    required this.answer,
+    this.answerType,
+    this.hint,
+    this.example,
+  });
+
+  final String? answerType;
+  final String? hint;
+  final String answer;
+  final String? example;
+}
+
+//TODO добавить sealed, когда добавится третий-четвёртый подтип или понадобится строгий switch
+// TODO Equatable, когда начнутся проблемы с сравнениями/перерисовками. сравнивать по айди и так можно но наверно лучше перестраховаться
